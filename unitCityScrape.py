fa6903eb2d68a47f319f796f54013c55d16a007a
Nathan Wong <nwong@courthouse-technologies.com>
2022-04-23 11:21:56 -0700

    Add Process folder and address = file name

--- unitCityScrape.py
+++ unitCityScrape.py
@@ -18,38 +18,45 @@
 ## https://maps.vancouver.ca/property/#
 ## https://gis.burnaby.ca/burnabymap/index.html
 
-# 4. Save html data in a txt file
-## 1234WilsonSt.txt
+# 4. Save html data in a txt file in "Process" folder. Name file the same as address so program can match the correct address if multiple addresses are in the file.
+## Process\1234 Wilson.txt
 
 # 5. Run script in Powershell/Terminal
 ## cd [folder location of app]
-## py unitCityScrape.py [path/filename.txt] --city [[V]ancouver (default) or [B]urnaby]
+## py unitCityScrape.py [address] --city [[V]ancouver (default) or [B]urnaby]
+## Examples:
+## py unitCityScrape.py '1234 Wilson'
+## py unitCityScrape.py '5555 McKay' --city B
 
 # 6. Unit numbers will output to [filename]_unit.txt
 
 import argparse
 import io
 import os
+import pathlib
 
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A utility to scrape units from Vancouver/Burnaby City Maps')
-    parser.add_argument('infile_path', help='Input File Path')
+    parser.add_argument('infile_path', help='Address/File name')
     parser.add_argument('--city', help='City ([V]ancouver or [B]urnaby)')
     parser.add_argument('--address', help='Specify Address')
     args = parser.parse_args()
     
-    infile = io.open(args.infile_path, mode="r", encoding="utf-8-sig")
+    folder = "Process/"
+    infile = io.open(folder + args.infile_path + ".txt", mode="r", encoding="utf-8-sig")
     file = os.path.splitext(os.path.basename(args.infile_path))
-    outfile = io.open(file[0] + '_units' + file[1], mode="w", encoding="utf-8-sig")
+
+    pathlib.Path(folder+'Complete').mkdir(parents=True, exist_ok=True) 
+    outfile = io.open(folder + "Complete/" + file[0] + '_units.txt' + file[1], mode="w", encoding="utf-8-sig")
     
     html_doc = infile.read()
     soup = BeautifulSoup(html_doc, 'html.parser')
     #print(soup.prettify())
 
     city = args.city and args.city.upper() or args.city
-    address = args.address and args.address.upper() or ''
+    address = args.infile_path and args.infile_path.upper() # or args.address and args.address.upper()
 
     print(address)
 
@@ -71,7 +78,6 @@ if __name__ == '__main__':
             if address in address2:				
                 print(unit + " - " + address2)
                 outfile.write(unit + '\n')
-                #prevUnit = unit 
 
     #Burnaby
     elif city == 'B':
